60bfe629d937c5b1e8b37531436895812f3e6e20
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-10-09 16:56:50 +0200

    ntn

--- src/app/complex-form/components/complex-form/complex-form.component.ts
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-complex-form',
@@ -9,17 +9,57 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ComplexFormComponent implements OnInit {
   
   mainForm! : FormGroup;
+  personalInfoForm!:FormGroup;
+  contactPreferenceCtrl!: FormControl;
+  phoneCtrl!:FormControl;
+  emailCtrl!: FormControl;
+  confirmEmailCtrl!: FormControl;
+  emailForm!: FormGroup;
+  passwordCtrl!: FormControl;
+  confirmPasswordCtrl!: FormControl;
+  loginInfoForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initFormControls();
     this.initMainForm();
   }
 
-  initMainForm():void{
-    this.mainForm = this.formBuilder.group({});
+  private initMainForm():void{
+      this.mainForm = this.formBuilder.group({
+          personalInfo: this.personalInfoForm,
+          contactPreference: this.contactPreferenceCtrl,
+          email: this.emailForm,
+          phone: this.phoneCtrl,
+          loginInfo: this.loginInfoForm
+      });
   }
 
-  onSubmitForm(){}
+  private initFormControls(){
+    this.personalInfoForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    })
+    this.contactPreferenceCtrl = this.formBuilder.control('email');
+    this.phoneCtrl = this.formBuilder.control('');
+    this.emailCtrl = this.formBuilder.control('');
+    this.confirmEmailCtrl = this.formBuilder.control('');
+    this.emailForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      confirm:this.confirmEmailCtrl
+    })
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control('', Validators.required);
+    this.loginInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl
+    })
+  }
+
+  onSubmitForm(){
+    console.log(this.mainForm.value)
+  }
 
 }
