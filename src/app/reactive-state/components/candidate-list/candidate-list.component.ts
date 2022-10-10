ed6d401e0ffc5438d9cec063bb6c47812ecdbc21
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-10-10 06:14:39 +0200

    ntn

--- src/app/reactive-state/components/candidate-list/candidate-list.component.ts
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, map, startWith, combineLatest } from 'rxjs';
 import { Candidate } from '../../models/candidate.model';
 import { CandidateService } from '../../services/candidate.service';
+import { CandidateSearchType } from '../../enum/candidate-search-type.enum'
 
 
 
@@ -20,6 +21,10 @@ export class CandidateListComponent implements OnInit {
 
   searchCtrl!: FormControl;
   searchTypeCtrl!: FormControl;
+  searchTypeOptions!: {
+    value: CandidateSearchType,
+    label: string
+  }[];
 
   constructor(private candidateService: CandidateService, private formBuilder: FormBuilder) { }
 
@@ -29,13 +34,32 @@ export class CandidateListComponent implements OnInit {
     this.candidateService.getCandidatesFromServer();
   }
   
-  private initObservables(){
-    this.isLoading$ = this.candidateService.loading$;
-    this.candidates$ = this.candidateService.candidates$;
+  private initForm(){
+    this.searchCtrl = this.formBuilder.control('');
+    this.searchTypeCtrl = this.formBuilder.control(CandidateSearchType.LASTNAME);
+    this.searchTypeOptions = [
+      { value: CandidateSearchType.LASTNAME, label: 'Lastname' },
+      { value: CandidateSearchType.FIRSTNAME, label: 'Firstname' },
+      { value: CandidateSearchType.COMPANY, label: 'Company' },
+    ]
   }
 
-  initForm(){
-    this.searchCtrl = this.formBuilder.control('')
+  private initObservables(){
+    this.isLoading$ = this.candidateService.loading$;
+    const search$ = this.searchCtrl.valueChanges.pipe(
+      startWith(this.searchCtrl.value), 
+      map(value => value.toLowerCase())
+    );
+    const searchType$: Observable<CandidateSearchType> = this.searchTypeCtrl.valueChanges.pipe(
+      startWith(this.searchTypeCtrl.value)
+    )
+    this.candidates$ = combineLatest([
+      search$,
+      searchType$,
+      this.candidateService.candidates$
+    ]).pipe(
+      map(([search, searchType, candidates]) => candidates.filter(candidate => candidate[searchType].toLowerCase().includes(search as string))))
   }
+
 
 }
--- src/app/reactive-state/services/candidate.service.ts
+++ src/app/reactive-state/services/candidate.service.ts
@@ -50,4 +50,8 @@ export class CandidateService {
     );
   }
 
+  refuseCandidate(id:number):void{
+    
+  }
+
 }
